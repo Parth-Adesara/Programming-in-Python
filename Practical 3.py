8310334b605f8e9a6930812a41c9a2f67cddf456
Parth-Adesara <90500353+Parth-Adesara@users.noreply.github.com>
2022-02-24 22:36:18 +0530

    Add files via upload

---
+++ Practical 3.py
@@ -0,0 +1,14 @@
+p = int(input())
+num_list = list(map(int, input().split()))
+num_count = dict()
+
+for i in num_list:
+    if i in num_count.keys():
+        num_count[i] += 1
+    else:
+        num_count[i] = 1
+
+for x in num_count.keys():
+    if num_count[x] != p:
+        print(x)
+        break
